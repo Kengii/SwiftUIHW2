6342363488a07f25499d4bd624be2f28fc09e184
Kengii <97613101+Kengii@users.noreply.github.com>
2022-04-03 11:54:57 +0300

    Project Created

---
+++ SwiftUIHW2/SwiftUIHW2/SwiftUIHW2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHW2App.swift
+//  SwiftUIHW2
+//
+//  Created by Владимир Данилович on 3.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
